9986a01cbd3ce1692e7a147fe2eec26ad8aabb17
Pg <palla.pg@gmail.com>
2019-06-25 16:17:33 +0000

    app/assets/javascripts/application.js
    add jquery function to cycle between divs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,23 @@
 //= require_tree
 
 //= require chartkick
+$(function() {
+  setTimeout(function(){
+    $('.alert').fadeOut(800);
+  }, 1000);
+  
+  var divs = $('div[id^="content-"]').hide(),
+    i = 0;
+
+(function cycle() { 
+    divs.eq(i).fadeIn(400)
+              .delay(2500)
+              .fadeOut(400, cycle);
+
+    i = ++i % divs.length; // increment i, 
+                           //   and reset to 0 when it equals divs.length
+})();
+  
+});
+
+
