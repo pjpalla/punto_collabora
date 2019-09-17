8064ff1bb7295d716c336c2e719600f7bedf9281
Pg <palla.pg@gmail.com>
2019-09-17 09:40:28 +0000

    add method get_choice to adapt the color of the pipeline to the button pressed to make a choice

--- app/helpers/advices_helper.rb
+++ app/helpers/advices_helper.rb
@@ -1,13 +1,25 @@
 module AdvicesHelper
     
-        def session_cleaner
-        
-        advice_keys = session.keys.select{|key| key.to_s.match(/^advice*/)}
-        advice_keys.each do |key|
-            session.delete(key)
-        end    
+def session_cleaner
+    advice_keys = session.keys.select{|key| key.to_s.match(/^advice*/)}
+    advice_keys.each do |key|
+        session.delete(key)
     end    
+end
 
+def get_choice(choice)
+    res = "danger"
+    if choice == "drugs"
+        res = "primary"
+    elsif choice == "health system"
+        res = "warning"
+    elsif choice == "shopping"
+        res = "success"
+    else    
+        res = "danger"
+    end
+    res
+end
     
     
     
