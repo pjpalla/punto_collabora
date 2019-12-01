565c31b5a53a8be25c189e5416a6fbb86ae89018
Pg <palla.pg@gmail.com>
2019-12-01 22:41:18 +0000

    extende devise registration controller to skip no_require_authentication

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,12 +1,28 @@
-class MembersController < ApplicationController
+class MembersController < Devise::RegistrationsController
+   skip_before_action :require_no_authentication
+   before_action :authenticate_member!
+   prepend_before_action :authenticate_scope!, only: [ :new, :create, :edit, :update, :destroy]
+   prepend_before_action :set_minimum_password_length, only: [:new, :edit]
    
    def index
      @members = Member.all
    end
    
-   
+   #binding.pry
    def new
-       
-   end       
+      super       
+   end
+   
+   def create
+      super
+   end
+   
+   def edit
+      super
+   end
+   
+   def update
+      super
+   end   
 
 end    
